02fc7ad7d784477527fe59c4a6b968e3fe927624
Karin <128316482+labplane@users.noreply.github.com>
2023-10-05 10:51:17 +0000

    alterações

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <h2 className="titulo">Rick Sanchez</h2>
+        <p>alguma tag</p>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
+    </>
+  )
+}
+
+export default App
